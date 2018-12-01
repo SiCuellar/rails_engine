a9793008a1dde60af2775e354d14a5797d3d655a
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-01 15:12:24 -0700

    added further merchant functionality

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe "Merchants API" do
+
   it "sends a list of merchants" do
     create_list(:merchant, 5)
 
@@ -9,5 +10,15 @@ describe "Merchants API" do
 
     expect(response).to be_successful
     expect(merchants.count).to eq(5)
+  end
+
+  it "it can get one merchant by its id " do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
   end
 end
