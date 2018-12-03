69c1abbbf67f739524d66657b05671c011e21ecb
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-03 02:58:04 -0700

    adding bus intel

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,13 @@ class Item < ApplicationRecord
 
   belongs_to :merchant
   has_many :invoice_items
+
+  def self.most_sold(quantity)
+    Item.select('items.*, SUM(invoice_items.quantity) AS number_sold')
+    .joins(:invoice_items,invoices: [:transactions])
+    .where(transactions: {result: 'success'})
+    .group('items.id')
+    .order('number_sold DESC')
+    .limit(quantity)
+  end
 end
