1d510cb963092db0e059bf936a29d84e6daea1f2
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-03 00:21:13 -0700

    added invoice relationships

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-    
+
       scope module: 'merchants' do
         resources :merchants, only: [:index, :show] do
           get '/items', to: 'items#index'
@@ -12,31 +12,35 @@ Rails.application.routes.draw do
       end
       resources :merchants, only: [:index, :show]
 
-      namespace :items do
-        # get '/find', to: 'item_find#show'
-        # get '/find_all', to: 'item_find#index'
-      end
+
       resources :items, only: [:index, :show]
+
 
       resources :transactions, only: [:index, :show]
 
-      # namespace :customers do
-      #   get '/find', to: 'customer_find#show'
-      #   get '/find_all', to: 'customer_find#index'
-      # end
+
       resources :customers, only: [:index, :show]
 
-      # namespace :invoice_items do
-      #   get '/find', to: 'invoice_item_find#show'
-      #   get '/find_all', to: 'invoice_item_find#index'
-      # end
+
       resources :invoice_items, only: [:index, :show]
 
       # namespace :invoices do
       #   get '/find', to: 'invoice_find#show'
       #   get '/find_all', to: 'invoice_find#index'
+      #   get '/random', to: 'invoice_random#show'
       # end
+      scope module: 'invoices' do
+        resources :invoices do
+          get '/transactions', to: 'transactions#index'
+          get '/invoice_items', to: 'invoice_items#index'
+          get '/items', to: 'items#index'
+          get '/customer', to: 'customer#show'
+          get '/merchant', to: 'merchant#show'
+        end
+      end
       resources :invoices, only: [:index, :show]
+
+
 
     end
   end
