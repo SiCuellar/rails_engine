bb3fe3ddc19a5a0f86852c1f6ee25b69048b2081
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-02 17:13:54 -0700

    having some serializer issuea agian

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,4 +1,4 @@
-class Ap1::V1::CustomersController < ApplicationController
+class Api::V1::CustomersController < ApplicationController
 
   def index
     render json: CustomerSerializer.new(Customer.all)
